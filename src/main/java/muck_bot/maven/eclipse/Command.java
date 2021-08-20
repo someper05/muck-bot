8892e2da153231df036475ff0677e06d91fe6075
Tyler Pursell <tjp4674@truman.edu>
2021-08-19 20:58:34 -0500

    Change main code to use commands interface

---
+++ src/main/java/muck_bot/maven/eclipse/Command.java
@@ -0,0 +1,8 @@
+package muck_bot.maven.eclipse;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+
+public interface Command
+{
+    void execute(MessageCreateEvent event);
+}
--- src/main/java/muck_bot/maven/eclipse/Main.java
+++ src/main/java/muck_bot/maven/eclipse/Main.java
@@ -1,26 +1,38 @@
 package muck_bot.maven.eclipse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.MessageChannel;
 
 public class Main 
 {
-    public static void main( String[] args )
-    {
+    private static final Map<String, Command> COMMANDS = new HashMap<>();
+    public static void main(String[] args)
+    {	
 	final DiscordClient client = DiscordClient.create(new BotToken().getToken());
 	final GatewayDiscordClient gateway = client.login().block();
-	gateway.on(MessageCreateEvent.class).subscribe(event -> {
-	    final Message message = event.getMessage();	    
-	    if ("!ping".equals(message.getContent())) 
-	    {		
-	        final MessageChannel channel = message.getChannel().block();
-	        
-	        channel.createMessage("Pong!").block();
-	    }
-	 });
-	 gateway.onDisconnect().block();
+	gateway.getEventDispatcher().on(MessageCreateEvent.class)
+	    .subscribe(event -> {
+	        final String content = event.getMessage().getContent(); 
+	        for (final Map.Entry<String, Command> entry : COMMANDS.entrySet())
+	        {
+	            if (content.startsWith('!' + entry.getKey()))
+	            {
+	                entry.getValue().execute(event);
+	                break;
+	            }
+	        }
+	    });
+	gateway.onDisconnect().block();
     }
+    
+    static 
+    {
+	COMMANDS.put("ping", event -> event.getMessage().getChannel().block()
+	        .createMessage("Pong!").block());
+    }
+   
 }
