6a0b22269158d1274eab6042c9e11dde76ded289
Tyler Pursell <tjp4674@truman.edu>
2021-08-21 11:36:35 -0500

    Clean up main file

--- src/main/java/muck_bot/music/TrackScheduler.java
+++ src/main/java/muck_bot/music/TrackScheduler.java
@@ -2,11 +2,12 @@ package muck_bot.music;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
-public final class TrackScheduler implements AudioLoadResultHandler
+public final class TrackScheduler extends AudioEventAdapter implements AudioLoadResultHandler
 {
     private final AudioPlayer player;
 
@@ -19,28 +20,28 @@ public final class TrackScheduler implements AudioLoadResultHandler
     {
 	return player;
     }
-
+    
     @Override
     public void trackLoaded(final AudioTrack track)
     {
-	player.playTrack(track);
+	player.playTrack(track);;
     }
 
     @Override
     public void playlistLoaded(final AudioPlaylist playlist)
-    {
+    {	
 
     }
 
     @Override
     public void noMatches()
     {
-
+	System.out.println("No matches found.");
     }
 
     @Override
     public void loadFailed(final FriendlyException exception)
     {
-
+	System.out.println(exception.getMessage());
     }
 }
